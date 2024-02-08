33cfe635a09c537231d894503e83b5e934240b09
Karanbohara01 <karanbohara216@gmail.com>
2024-02-09 00:09:20 +0545

    all-orders-screen

---
+++ lib/screens/user-panel/all-orders-screen.dart
@@ -0,0 +1,134 @@
+// ignore_for_file: file_names, prefer_const_constructors, avoid_unnecessary_containers, prefer_const_literals_to_create_immutables, sized_box_for_whitespace, avoid_print
+
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_application_e_comm/utils/app-constant.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+
+import '../../controllers/cart-price-controller.dart';
+import '../../models/order-model.dart';
+
+class AllOrdersScreen extends StatefulWidget {
+  const AllOrdersScreen({super.key});
+
+  @override
+  State<AllOrdersScreen> createState() => _AllOrdersScreenState();
+}
+
+class _AllOrdersScreenState extends State<AllOrdersScreen> {
+  User? user = FirebaseAuth.instance.currentUser;
+  final ProductPriceController productPriceController =
+      Get.put(ProductPriceController());
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: AppConstants.primaryColor,
+        title: Text('All Orders'),
+      ),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance
+            .collection('orders')
+            .doc(user!.uid)
+            .collection('confirmOrders')
+            .snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) {
+            return Center(
+              child: Text("Error"),
+            );
+          }
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Container(
+              height: Get.height / 5,
+              child: Center(
+                child: CupertinoActivityIndicator(),
+              ),
+            );
+          }
+
+          if (snapshot.data!.docs.isEmpty) {
+            return Center(
+              child: Text("No products found!"),
+            );
+          }
+
+          if (snapshot.data != null) {
+            return Container(
+              child: ListView.builder(
+                itemCount: snapshot.data!.docs.length,
+                shrinkWrap: true,
+                physics: BouncingScrollPhysics(),
+                itemBuilder: (context, index) {
+                  final productData = snapshot.data!.docs[index];
+                  OrderModel orderModel = OrderModel(
+                    productId: productData['productId'],
+                    categoryId: productData['categoryId'],
+                    productName: productData['productName'],
+                    categoryName: productData['categoryName'],
+                    salePrice: productData['salePrice'],
+                    fullPrice: productData['fullPrice'],
+                    productImages: productData['productImages'],
+                    deliveryTime: productData['deliveryTime'],
+                    isSale: productData['isSale'],
+                    productDescription: productData['productDescription'],
+                    createdAt: productData['createdAt'],
+                    updatedAt: productData['updatedAt'],
+                    productQuantity: productData['productQuantity'],
+                    productTotalPrice: double.parse(
+                        productData['productTotalPrice'].toString()),
+                    customerId: productData['customerId'],
+                    status: productData['status'],
+                    customerName: productData['customerName'],
+                    customerPhone: productData['customerPhone'],
+                    customerAddress: productData['customerAddress'],
+                    customerDeviceToken: productData['customerDeviceToken'],
+                  );
+
+                  //calculate price
+                  productPriceController.fetchProductPrice();
+                  return Card(
+                    elevation: 5,
+                    color: AppConstants.textColor,
+                    child: ListTile(
+                      leading: CircleAvatar(
+                        backgroundColor: AppConstants.primaryColor,
+                        backgroundImage:
+                            NetworkImage(orderModel.productImages[0]),
+                      ),
+                      title: Text(orderModel.productName),
+                      subtitle: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Text(orderModel.productTotalPrice.toString()),
+                          SizedBox(
+                            width: 10.0,
+                          ),
+                          orderModel.status != true
+                              ? Text(
+                                  "Pending..",
+                                  style: TextStyle(color: Colors.green),
+                                )
+                              : Text(
+                                  "Deliverd",
+                                  style: TextStyle(color: Colors.red),
+                                )
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            );
+          }
+
+          return Container();
+        },
+      ),
+    );
+  }
+}
