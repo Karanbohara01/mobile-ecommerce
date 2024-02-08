abd500885b90afe9840b6364f3c157180ccad83f
Karanbohara01 <karanbohara216@gmail.com>
2024-02-08 21:47:09 +0545

    carousel-controller

---
+++ lib/controllers/banners-controller.dart
@@ -0,0 +1,33 @@
+// ignore_for_file: camel_case_types, file_names, unnecessary_overrides, unused_local_variable, avoid_print
+
+
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:get/get_rx/src/rx_types/rx_types.dart';
+import 'package:get/get_state_manager/src/simple/get_controllers.dart';
+
+class bannerController extends GetxController {
+  RxList<String> bannerUrls = RxList<String>([]);
+
+  @override
+  void onInit() {
+    super.onInit();
+    fetchBannersUrls();
+  }
+
+  //fetch banners
+  Future<void> fetchBannersUrls() async {
+    try {
+      QuerySnapshot bannersSnapshot =
+          await FirebaseFirestore.instance.collection('banners').get();
+
+      if (bannersSnapshot.docs.isNotEmpty) {
+        bannerUrls.value = bannersSnapshot.docs
+            .map((doc) => doc['imageUrl'] as String)
+            .toList();
+      }
+    } catch (e) {
+      print("error: $e");
+    }
+  }
+}
--- lib/widgets/banner-widget.dart
+++ lib/widgets/banner-widget.dart
@@ -1,16 +1,14 @@
 // ignore_for_file: file_names, unused_field, avoid_unnecessary_containers, prefer_const_constructors
 import 'package:cached_network_image/cached_network_image.dart';
-import 'package:carousel_slider/carousel_controller.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
-import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
+
+import '../controllers/banners-controller.dart';
 
 class BannerWidget extends StatefulWidget {
   const BannerWidget({super.key});
-
   @override
   State<BannerWidget> createState() => _BannerWidgetState();
 }
